200fce7c12e76c9544e35be64a43ef76b1019365
Avuwep <123604936+Avuwep@users.noreply.github.com>
2026-01-26 22:44:12 -0500

    Auton Closer

--- src/main.cpp
+++ src/main.cpp
@@ -181,21 +181,88 @@ void autonomous() {
 
 
 void opcontrol() {
-    right_motors.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    left_motors.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    // loop forever
-    Descore.extend();
+        chassis.setPose(-48,-12,180);
+    chassis.moveToPose(-48, -49,270, 3000);
+    chassis.turnToHeading(270,2000);
     loaderFork.extend();
-    while (true) {
-        // get left y and right x positions
-        int leftY = (controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y))*.8;
-        int rightX = (controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X))*.8;
+    intake();
+    pros::delay(500);
+    chassis.moveToPoint(-75,-46.5,3000);
+    pros::delay(500);
+    chassis.moveToPoint(75,-46.5,150, {.forwards = false});
+    pros::delay(200);
+    chassis.moveToPoint(-75,-46.5,3000);
+    chassis.moveToPoint(75,-46.5,150, {.forwards = false});
+    pros::delay(200);
+    chassis.moveToPoint(-75,-46.5,3000);
+    pros::delay(4750);
+    chassis.moveToPoint(-48,-50,3000, {.forwards = false, .minSpeed = 72, .earlyExitRange = 8});
+    chassis.moveToPoint(-27.5, -62, 3000, {.forwards = false});
+    chassis.moveToPoint(45, -62, 4000, {.forwards = false});
+    chassis.turnToHeading(0,500);
+    chassis.moveToPoint(48,-48,2000);
+    chassis.turnToHeading(90,500);
+    chassis.moveToPoint(0,-48,8000,{.forwards = false}, true);
+    pros::delay(1000);
+    topOuttake();
+    pros::delay(5000);
+    intake();
+    chassis.moveToPose(80,-46.5,90,1500,{}, true);
+    pros::delay(1500);
+    chassis.moveToPose(-80,-46.5,90,150, {.forwards = false});
+    chassis.moveToPose(80,-46.5,90,5000);
+    chassis.moveToPose(-80,-46.5,90,150, {.forwards = false});
+    chassis.moveToPose(80,-46.5,90,5000);
+    chassis.moveToPoint(0,-48,8000,{.forwards = false});
+    pros::delay(1000);
+    topOuttake();
+    pros::delay(5000);
+    intake();
+    chassis.moveToPoint(40,-45,2000);
+    chassis.turnToHeading(0,1000);
+    chassis.moveToPoint(40,48,2500);
+    chassis.turnToHeading(90, 1000);
+    chassis.moveToPoint(70, 48, 3000, {}, false);
+    pros::delay(1000);
+    loaderFork.retract();
+    chassis.moveToPoint(-70,48,100,{.forwards = false});
+    chassis.moveToPoint(70, 48, 5000, {}, false);
+    chassis.moveToPoint(40,48,2500, {.forwards = false, .minSpeed = 72, .earlyExitRange = 8});
+    chassis.moveToPoint(25,60,1000);
+    chassis.moveToPoint(-48,60,5000);
+    chassis.moveToPoint(-48,48,2000);
+    chassis.moveToPoint(48,48,2000, {.forwards = false}, true);
+    pros::delay(750);
+    topOuttake();
+    loaderFork.extend();
+    pros::delay(7000);
+    intake();
+    chassis.moveToPoint(-75,50,2000);
+    chassis.moveToPoint(75,50,150, {.forwards = false});
+    chassis.moveToPoint(-75,50,2000);
+    chassis.moveToPoint(75,50,150, {.forwards = false});
+    chassis.moveToPoint(-75,50,7000);
+    chassis.moveToPoint(75,50,7000, {.forwards = false},true);
+    pros::delay(750);
+    topOuttake();
+    pros::delay(7000);
+    chassis.moveToPoint(-63,21,3000);
+    chassis.moveToPoint(-64,0,2000,{.minSpeed = 127});
+    // right_motors.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    // left_motors.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+    // // loop forever
+    // Descore.extend();
+    // loaderFork.extend();
+    // while (true) {
+    //     // get left y and right x positions
+    //     int leftY = (controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y))*.8;
+    //     int rightX = (controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X))*.8;
 
-        // move the robot
-        chassis.arcade(leftY, rightX);
+    //     // move the robot
+    //     chassis.arcade(leftY, rightX);
 
-        controllerCode();
+    //     controllerCode();
 
-        pros::delay(20);// small delay for loop timing
-    }
+    //     pros::delay(20);// small delay for loop timing
+    // }
 }
